0b406b5ae77a5f3114926d2920c4d6f8f3b15471
Ilias Garnier <igarnier@protonmail.com>
2023-07-25 07:40:33 +0200

    canonicalize polys

--- lib/poly.ml
+++ lib/poly.ml
@@ -1,22 +1,38 @@
 module Make (R : Intf.Ring_std) : sig
   include Intf.Ring_std with type t = R.t array
 
+  val canonical : t -> t
   val degree : t -> int
 end = struct
   type t = R.t array
 
   let zero = [| R.zero |]
+
+  let canonical p =
+    let len = Array.length p in
+    if len = 0 then zero
+    else
+      let first_nonzero = ref (len - 1) in
+      let exception Break in
+      (try
+         for i = len - 1 downto 0 do
+           if R.equal p.(i) R.zero then first_nonzero := i - 1 else raise Break
+         done
+       with Break -> ());
+      if !first_nonzero = -1 then zero
+      else Array.init (!first_nonzero + 1) (fun i -> p.(i))
+
   let degree p = Array.length p - 1
   let one = [| R.one |]
   let get p i = if i >= Array.length p then R.zero else p.(i)
 
   let add p1 p2 =
     let len = Int.max (Array.length p1) (Array.length p2) in
-    Array.init len (fun i -> R.add (get p1 i) (get p2 i))
+    Array.init len (fun i -> R.add (get p1 i) (get p2 i)) |> canonical
 
   let sub p1 p2 =
     let len = Int.max (Array.length p1) (Array.length p2) in
-    Array.init len (fun i -> R.sub (get p1 i) (get p2 i))
+    Array.init len (fun i -> R.sub (get p1 i) (get p2 i)) |> canonical
 
   let neg p = Array.map R.neg p
 
@@ -32,7 +48,12 @@ end = struct
         coeffs.(target) <- R.add coeffs.(target) (R.mul coeff1 coeff2)
       done
     done;
-    coeffs
+    coeffs |> canonical
+
+  let div (p1 : t) (p2 : t) =
+    let deg_p1 = degree p1 in
+    let deg_p2 = degree p2 in
+    if deg_p1 < deg_p2 then (zero, p1) else assert false
 
   let equal p1 p2 =
     let deg_p1 = degree p1 in
